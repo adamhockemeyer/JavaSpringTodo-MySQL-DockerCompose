52e97b4444ea498cfe0183a8ff8fee41ae3ca8b5
Adam Hockemeyer <hock2k5@gmail.com>
2019-10-28 14:47:17 -0400

    Update schema.sql

--- JavaTodoService-server/src/main/resources/schema.sql
+++ JavaTodoService-server/src/main/resources/schema.sql
@@ -1,7 +1,7 @@
   
 create table if not exists todos
 (
-   id integer not null AUTO_INCREMENT,
+   id integer not null auto_increment,
    title varchar(255) not null, 
    completed boolean not null,
    primary key(id)
